a1351b366ce2b2b597dd334a5a784dedca512d5e
Vivek <vivekaxl@gmail.com>
2016-04-03 20:40:43 -0400

    AsdsA

--- Supercharger_post_process/process.py
+++ Supercharger_post_process/process.py
@@ -3,7 +3,8 @@ from __future__ import division
 import matplotlib.pyplot as plt
 
 algorithms = ["NSGAII", "SPEA2", "GALE"]
-problems = ["POM3A", "POM3B", "POM3C", "POM3D", "xomo_ground", "xomo_flight", "xomo_all", "xomoo2", "xomo_osp"]
+# problems = ["POM3A", "POM3B", "POM3C", "POM3D", "xomo_ground", "xomo_flight", "xomo_all", "xomoo2", "xomo_osp"]
+problems = ["xomo_flight"]
 
 from os import listdir
 import pandas as pd
